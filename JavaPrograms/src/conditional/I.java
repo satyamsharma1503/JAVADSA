460b03e71e38e1e9829f527aec7d58d37dc092cf
satyamsharma1503 <ss9655278@gmail.com>
2024-07-24 11:26:28 +0530

    check number is even number to continue statement

---
+++ JavaPrograms/src/conditional/I.java
@@ -0,0 +1,18 @@
+package conditional;
+
+public class I {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <=10; i++) {
+            if(i%2==0){
+                System.out.print("even "+i);
+                continue;
+            }
+                System.out.println();
+            }
+
+        }
+    }
+
+
+
