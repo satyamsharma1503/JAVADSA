4e3195383e0a30ffab9d01407f3b5c928777f155
satyamsharma1503 <ss9655278@gmail.com>
2024-07-25 17:43:53 +0530

    time complexity

--- JavaPrograms/src/conditional/TimeComplexityDebug.java
+++ JavaPrograms/src/conditional/TimeComplexityDebug.java
@@ -2,6 +2,13 @@ package conditional;
 
 public class TimeComplexityDebug {
     public static void main(String[] args) {
-        System.out.println("fvgdhkjdbj");
+        int arr[]={1,7,9,11,12};
+        int sum =0;
+        for(int i=0; i<arr.length; i++ ){
+            sum=sum+arr[i];
+
+        }
+        int avg =(sum/arr.length);
+        System.out.println(avg);
     }
 }
